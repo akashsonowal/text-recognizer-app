1b6f04e1e2dba606bed2ffb3b3235eb8692831ef
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-01-14 14:21:34 +0530

    Update paragraph_text_recognizer.py

--- text_recognizer/paragraph_text_recognizer.py
+++ text_recognizer/paragraph_text_recognizer.py
@@ -9,3 +9,19 @@ Example usage of script:
   python text_recognizer/paragraph_text_recognizer.py \
     https://fsdl-public-assets.s3-us-west-2.amazonaws.com/paragraphs/a01-077.png
 """
+import argparse
+
+
+
+
+def main():
+  parser = argparse.ArgumentParser(description=__doc__.split("\n")[0])
+  parser.add_argument("filename", type=str, help="Name for an image file. This can be a local path, a URL, a URI from AWS/GCP/Azure storage, an HDFS path, or any other resource locator supported by the smart_open library.")
+  args = parser.parse_args()
+  
+  text_recognizer = Paragraph_Text_Recognizer()
+  pred_str = text_recognizer.predict(args.filename)
+  print(pred_str)
+
+if __name__ == "__main__":
+  main()
