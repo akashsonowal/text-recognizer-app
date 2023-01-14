1a657d16224913190b60bba77d76fabc06d1d1ee
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-01-14 14:27:23 +0530

    Update paragraph_text_recognizer.py

--- text_recognizer/paragraph_text_recognizer.py
+++ text_recognizer/paragraph_text_recognizer.py
@@ -4,7 +4,7 @@ Detects a paragraph of text in an input image.
 Example usage of script:
 
   python text_recognizer/paragraph_text_recognizer.py \
-    text_recognizer/tests/support/paragraphs/a01-077.png
+    data/tests/a01-077.png
   
   python text_recognizer/paragraph_text_recognizer.py \
     https://fsdl-public-assets.s3-us-west-2.amazonaws.com/paragraphs/a01-077.png
