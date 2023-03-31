ee56ade4a77181014885a8fe883fe9a6fd8dc4c2
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-03-31 07:21:08 +0530

    update data

---
+++ text_recognizer/data/__init__.py
@@ -0,0 +1,13 @@
+"""Module containing submodules for each dataset.
+
+Each dataset is defined as a class in that submodule.
+
+The dataset should have a .config method that returns any 
+configuration information needed by the model.
+
+Most datasets define their constants in a submodule
+of the metadata module that is parallel to this one in the 
+hierarchy.
+"""
+from .util import BaseDataset
+
