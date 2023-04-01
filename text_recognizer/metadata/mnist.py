afecd3b65f5049609d5d3def2513639357157b0a
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-01 15:21:56 +0530

    Update mnist.py

--- text_recognizer/metadata/mnist.py
+++ text_recognizer/metadata/mnist.py
@@ -1,1 +1,12 @@
 """Metadata for the MNIST dataset."""
+from text_recognizer.metadata.shared as shared
+
+DOWNLOADED_DATA_DIRNAME = shared.DOWNLOADED_DATA_DIRNAME
+
+DIMS = (1, 28, 28)
+OUTPUT_DIMS = (1,)
+MAPPING = list(range(10))
+
+TRAIN_SIZE = 55000
+VAL_SIZE = 5000
+
