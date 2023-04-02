ef1fab9e268868b6bf87a24f58256220ab2276d1
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-02 08:12:29 +0530

    Update util.py

--- text_recognizer/data/util.py
+++ text_recognizer/data/util.py
@@ -75,7 +75,3 @@ def resize_image(image: Image.Image, scale_factor: int) -> Image.Image:
     if scale_factor == 1:
       return image
     return image.resize((image.width // scale_factor, image.height // scale_factor), resample=Image.BILINEAR)
-      
-      
-  
-  
