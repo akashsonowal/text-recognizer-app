bd8e7cf66d4fc21457662b11e6ed8acc9af76aef
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-02 08:10:32 +0530

    Update util.py

--- text_recognizer/data/util.py
+++ text_recognizer/data/util.py
@@ -70,8 +70,11 @@ def convert_strings_to_labels():
 def split_dataset():
   pass
 
-def resize_image():
-  pass
+def resize_image(image: Image.Image, scale_factor: int) -> Image.Image:
+    """Resize image by scale factor."""
+    if scale_factor == 1:
+      return image
+    return image.resize((image.width // scale_factor, image.height // scale_factor), resample=Image.BILINEAR)
       
       
   
