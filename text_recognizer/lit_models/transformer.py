bf120a42db75663d18fc3bf5d0199f5cb0a633a2
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-03 11:59:40 +0530

    Create transformer.py

---
+++ text_recognizer/lit_models/transformer.py
@@ -0,0 +1,19 @@
+"""An encoder-decoder Transformer model"""
+from typing import List, Sequence
+
+import torch
+
+from .base import BaseImageToTextLitModel
+from .util import replace_after
+
+class TransformerLitModel(BaseImageToTextModel):
+  """
+  """
+  def forward():
+    pass
+  
+  def teacher_forward():
+    pass
+  
+  def training_step(self, batch, batch_idx):
+    
