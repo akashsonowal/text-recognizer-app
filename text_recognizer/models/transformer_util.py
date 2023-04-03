4bf521c58aa0463ac2fb17a27ec8af8530a3fe95
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-03 12:40:13 +0530

    Update transformer_util.py

--- text_recognizer/models/transformer_util.py
+++ text_recognizer/models/transformer_util.py
@@ -1,7 +1,8 @@
 """Position Encoding and other utilities for Transformers."""
 import math
 
-
+class PositionEncoding(nn.Module):
+  pass
 
 def generate_square_subsequent_mask(size: int) -> torch.Tensor:
   return mask
