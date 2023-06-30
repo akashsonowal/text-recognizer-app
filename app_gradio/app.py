ed550682a9791d37a0a5f43cfa4f692f40f47d80
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-30 18:53:10 +0530

    update

--- app_gradio/app.py
+++ app_gradio/app.py
@@ -19,6 +19,22 @@ from app_gradio.s3_util import make_unique_bucket_name
 from text_recognizer.paragraph_text_recognizer import ParagraphTextRecognizer 
 import text_recognizer.util as util 
 
+os.environ["CUDA_VISIBLE_DEVICES"] = "" # don't use gpu
+
+logging.basicConfig(level=logging.INFO)
+DEFAULT_APPLICATION_NAME = "text-recognizer"
+APP_DIR = Path(__file__).resolve().parent # what is the directory for this application?
+FAVICON = APP_DIR / "favicon.png" # path to a small image for display in browser tab and social media
+README = APP_DIR / "readme.md" # path to an app readme file in HTML/markdown
+
+DEFAULT_PORT = 11700
+
+def main(args):
+  predictor = PredictorBackend(url=args.model_url)
+  
+
+
+
 
 
 class PredictorBackend:
