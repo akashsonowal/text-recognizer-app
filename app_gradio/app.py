4121c9f6bdbf0164dc5d4aa09e153802c93fd30d
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-30 18:37:26 +0530

    update

--- app_gradio/app.py
+++ app_gradio/app.py
@@ -9,6 +9,16 @@ from typing import Callable
 import warnings 
 
 import gradio as gr 
+from PIL import ImageStat
+from PIL.Image import Image 
+import requests 
+
+from app_gradio.flagging import GantryImageToTextLogger, get_api_key 
+from app_gradio.s3_util import make_unique_bucket_name 
+
+from text_recognizer.paragraph_text_recognizer import ParagraphTextRecognizer 
+import text_recognizer.util as util 
+
 
 
 class PredictorBackend:
