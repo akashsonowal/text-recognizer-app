d8d7a48cbc43161b0d55e497fdaea2192121dca8
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-30 18:28:56 +0530

    update

--- app_gradio/app.py
+++ app_gradio/app.py
@@ -1,2 +1,15 @@
+"""Provide an image of handwritten text and get back out a string!"""
+import argparse 
+import json 
+import logging 
+import os 
+from pathlib import Path 
+from typing import Callable
+
+import warnings 
+
+import gradio as gr 
+
+
 class PredictorBackend:
   pass
