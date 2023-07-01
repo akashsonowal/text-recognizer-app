05edf06cc57da9a06562a497a8d39f2b5b121268
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-01 18:31:14 +0530

    update

--- app_gradio/flagging.py
+++ app_gradio/flagging.py
@@ -1,1 +1,13 @@
+import os 
+from typing import List, Optional, Union
+
+import gantry
+import gradio as gr 
+from gradio.components import Component
+from smart_open import open 
+
+from app_gradio import s3_util
+from text_recognizer.util import read_b64_string 
+
+
 
