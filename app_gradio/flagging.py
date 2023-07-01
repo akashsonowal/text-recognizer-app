b896db0ac1b64555f474b20a44d4e8fa6a0efb6d
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-01 18:37:35 +0530

    update

--- app_gradio/flagging.py
+++ app_gradio/flagging.py
@@ -9,5 +9,12 @@ from smart_open import open
 from app_gradio import s3_util
 from text_recognizer.util import read_b64_string 
 
+class GantryImageToTextLogger(gr.FlaggingCallback):
+    """A FlaggingCallback that logs flagged image-to-text data to Gantry via S3."""
+    pass 
 
+def get_api_key() -> Optional[str]:
+    """Convenience method for fetching the Gantry API key."""
+    api_key = os.environ.get("GANTRY_API_KEY")
+    return api_key
 
