b96f6f62b4bc0203cfc327fd717e6851989d981c
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-01 19:19:52 +0530

    update

--- app_gradio/flagging.py
+++ app_gradio/flagging.py
@@ -11,7 +11,36 @@ from text_recognizer.util import read_b64_string
 
 class GantryImageToTextLogger(gr.FlaggingCallback):
     """A FlaggingCallback that logs flagged image-to-text data to Gantry via S3."""
-    pass 
+    def __init__(self, application: str, version: Union[int, str, None] = None, api_key: Optional[str] = None):
+        """Logs image-to-text data that was flagged in Gradio to Gantry.
+
+        Images are logged to Amazon Web Services' Simple Storage Service (S3).
+
+        The flagging_dir provided to the Gradio interface is used to set the
+        name of the bucket on S3 into which images are logged.
+
+        See the following tutorial by Dan Bader for a quick overview of S3 and the AWS SDK
+        for Python, boto3: https://realpython.com/python-boto3-aws-s3/
+
+        See https://gradio.app/docs/#flagging for details on how
+        flagging data is handled by Gradio.
+
+        See https://docs.gantry.io for information about logging data to Gantry.
+
+        Parameters
+        ----------
+        application
+            The name of the application on Gantry to which flagged data should be uploaded.
+            Gantry validates and monitors data per application.
+        version
+            The schema version to use during validation by Gantry. If not provided, Gantry
+            will use the latest version. A new version will be created if the provided version
+            does not exist yet.
+        api_key
+            Optionally, provide your Gantry API key here. Provided for convenience
+            when testing and developing locally or in notebooks. The API key can
+            alternatively be provided via the GANTRY_API_KEY environment variable.
+        """
 
 def get_api_key() -> Optional[str]:
     """Convenience method for fetching the Gantry API key."""
