49fa43c766649425d22dfaaac2d7f81640a37195
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-02 18:46:34 +0530

    update

--- text_recognizer/data/emnist.py
+++ text_recognizer/data/emnist.py
@@ -1,1 +1,7 @@
+from text_recognizer.util import temporary_working_directory
+
+def _process_raw_dataset(filename: str, dirname: Path):
+    print("Unzipping EMNIST...")
+    with temporary_working_directory(dirname):
+        from scipy.io import loadmat
 
--- text_recognizer/util.py
+++ text_recognizer/util.py
@@ -1,4 +1,9 @@
 """Utility functions for text_recognizer module."""
+import base64
+import contextlib
+import hashlib
+
+
 import numpy as np 
 from PIL import Image 
 import smart_open
@@ -6,3 +11,13 @@ from tqdm import tqdm
 
 def read_image_pil():
   pass
+
+@contextlib.contextmanager
+def temporary_working_directory(working_dir: Union[str, Path]):
+  """Temporarily switches to a directory, then returns to the original directory on exit."""
+  curdir = os.getcwd()
+  os.chdir(working_dir)
+  try:
+    yield
+  finally:
+    os.chdir(curdir)
