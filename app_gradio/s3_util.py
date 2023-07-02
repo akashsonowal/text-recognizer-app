854d690bab196900dbff348f8775297d5fa6158b
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-02 11:57:07 +0530

    update

--- app_gradio/s3_util.py
+++ app_gradio/s3_util.py
@@ -7,3 +7,26 @@ import botocore
 S3_URL_FORMAT = "https://{bucket}.s3.{region}.amazonaws.com/{key}"
 S3_URI_FORMAT = "s3://{bucket}/{key}"
 
+s3 = boto3.resource("s3")
+
+def get_or_create_bucket(name):
+    """Gets an S3 bucket with boto3 or creates it if it doesn't exist."""
+    try: # try to create a bucket
+        name, response = _create_bucket(name)
+    except botocore.exceptions.ClientError as err:
+        # error handling from https://github.com/boto/boto3/issues/1195#issuecomment-495842252
+        status = err.response["ResponseMetadata"]["HTTPStatusCode"] # status codes identify particular errors
+
+        if status == 409:  # if the bucket exists already,
+            pass # we don't need to make it -- we presume we have the right permissions
+        else:
+            raise err 
+    
+    bucket = s3.Bucket(name)
+
+    return bucket
+    
+
+
+
+
