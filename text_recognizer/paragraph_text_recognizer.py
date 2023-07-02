dbf18e8a8081ce996c4819494cb5e9cdb233f76b
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-02 16:35:56 +0530

    update

--- text_recognizer/paragraph_text_recognizer.py
+++ text_recognizer/paragraph_text_recognizer.py
@@ -8,4 +8,21 @@ Example usage as a script:
   python text_recognizer/paragraph_text_recognizer.py \
     https://fsdl-public-assets.s3-us-west-2.amazonaws.com/paragraphs/a01-077.png
 """
+import argparse 
+from pathlib import Path
+from typing import Sequence, Union
 
+from PIL import Image 
+import torch 
+
+from text_recognizer import util
+from text_recognizer.stems.paragraph import ParagraphStem 
+
+STAGED_MODEL_DIRNAME = Path(__file__).resolve().parent / "artifacts" / "paragraph-text-recognizer"
+MODEL_FILE = "model.pt"
+
+def main():
+    text_recognizer = 
+
+if __name__ == "__main__":
+    main()
