95dc81201b9e43967b23d3167c11b52835b69ed9
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-02 18:50:09 +0530

    update

--- text_recognizer/data/iam.py
+++ text_recognizer/data/iam.py
@@ -1,1 +1,5 @@
-
+def _extract_raw_dataset(filename: str, dirname: Path) -> None:
+    print("Extracting IAM data")
+    with util.temporary_working_directory(dirname):
+        with zipfile.ZipFile(filename, 'r') as zip_file:
+            zip_file.extractall()
