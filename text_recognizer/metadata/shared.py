9a71e3ceeb9f96a60c1f2130e76ad576eef55989
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-03 08:50:49 +0530

    update

--- text_recognizer/metadata/shared.py
+++ text_recognizer/metadata/shared.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
-DATA_DIRNAME = Path(__file__).resolve().parents[2] / "data"
+DATA_DIRNAME = Path(__file__).resolve().parents[2] / "data" # two levels up from current dir
 DOWNLOADED_DATA_DIRNAME = DATA_DIRNAME / "downloaded"
