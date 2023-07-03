beaf3d51be692606477c5d8e8ba1526f39ce69f4
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-03 11:48:18 +0530

    update

--- text_recognizer/data/base_data_module.py
+++ text_recognizer/data/base_data_module.py
@@ -34,3 +34,39 @@ def _download_raw_dataset(metadata: Dict, dl_dirname: Path) -> Path:
   if sha256 != metadata["sha256"]:
     raise ValueError("Downloaded data file SHA-256 does not match that listed in metadata document.")
   return filename
+
+BATCH_SIZE = 128
+NUM_AVAIL_CPUS = len(os.sched_getaffinity(0))
+NUM_AVAIL_GPUS = torch.cuda.device_count()
+
+# sensible multiprocessing defaults: at most one worker per CPU
+DEFAULT_NUM_WORKERS = NUM_AVAIL_CPUS
+# but in distributed data parallel mode, we launch a training on each GPU, so must divide out to keep total at one worker per CPU
+DEFAULT_NUM_WORKERS = NUM_AVAIL_CPUS // NUM_AVAIL_GPUS if NUM_AVAIL_GPUS else DEFAULT_NUM_WORKERS
+
+class BaseDataModule(pl.LightningDataModule):
+  """Base for all of our LightningDataModules.
+
+  Learn more at about LDMs at https://pytorch-lightning.readthedocs.io/en/stable/extensions/datamodules.html
+  """
+  def __init__(self, args: argparse.Namespace = None) -> None:
+    super().__init__()
+    self.args = vars(args)
+  
+  def config():
+    pass 
+    
+  def prepare_data():
+    pass 
+
+  def setup():
+    pass
+  
+  def train_dataloader(self):
+    pass 
+  
+  def val_dataloader(self):
+    pass 
+  
+  def test_dataloader(self):
+    pass 
