6ac5705f4f09ab381d1bfaa211ecc0a57b9faab2
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-03 11:11:59 +0530

    update

--- text_recognizer/util.py
+++ text_recognizer/util.py
@@ -73,9 +73,9 @@ def split_and_validate_b64_string(b64_string):
 def encode_b64_image(image, format="png"):
   """Encode a PIL image as a base64 string."""
   _buffer = BytesIO() # bytes that live in memory
-  
-
-  pass 
+  image.save(_buffer, format=format) # but which we write to like a file
+  encoded_image = base64.b64encode(_buffer.getvalue()).decode("utf8")
+  return encoded_image
 
 def compute_sha256(filename: Union[Path, str]):
   """Return SHA256 checksum of a file."""
