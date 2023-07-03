e36ec037308981a2708e1238dcd8a77101b91dd1
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-03 15:08:34 +0530

    update

--- text_recognizer/data/base_data_module.py
+++ text_recognizer/data/base_data_module.py
@@ -68,11 +68,23 @@ class BaseDataModule(pl.LightningDataModule):
   def data_dirname(cls):
     return metadata.DATA_DIRNAME
   
+  @staticmethod
+  def add_to_argparse(parser):
+    parser.add_argument(
+      "--batch_size",
+      type=int,
+      default=BATCH_SIZE,
+      help=f"Number of examples to operate on per forward step. Default is {BATCH_SIZE}.",
+    )
+    parser.add_argument(
+      "--num_workers",
+      type=int,
+      default=DEFAULT_NUM_WORKERS,
+      help=f"Number of additional processes to load data. Default is {DEFAULT_NUM_WORKERS}."
+    )
+    return parser
   
-
-
-  
-  def config():
+  def config(self):
     pass 
 
   def prepare_data():
