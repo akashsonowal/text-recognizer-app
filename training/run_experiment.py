441345b91bb18d915f619ad54d1721023fa07a64
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-03 20:50:44 +0530

    update

--- training/run_experiment.py
+++ training/run_experiment.py
@@ -1,6 +1,27 @@
 """Experiment-running framework."""
 
 def main():
+  main():
+    """
+    Run an experiment.
+
+    Sample command:
+    ```
+    python training/run_experiment.py --max_epochs=3 --gpus='0,' --num_workers=20 --model_class=MLP --data_class=MNIST
+    ```
+
+    For basic help documentation, run the command
+    ```
+    python training/run_experiment.py --help
+    ```
+
+    The available command line args differ depending on some of the arguments, including --model_class and --data_class.
+
+    To see which command line args are available and read their documentation, provide values for those arguments
+    before invoking --help, like so:
+    ```
+    python training/run_experiment.py --model_class=MLP --data_class=MNIST --help
+    """
   pass
 
 if __name__ == "__main__":
