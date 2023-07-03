84841f7d3eab9f8aa0215d5bf11a7145911ba78a
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-03 08:19:44 +0530

    update

--- text_recognizer/data/base_data_module.py
+++ text_recognizer/data/base_data_module.py
@@ -1,2 +1,16 @@
+"""Base DataModule class."""
+import argparse 
+import os 
+from pathlib import Path 
+from typing import Collection, Dict, Optional, Tuple, Union
+
+import pytorch_lightning as pl 
+import torch 
+from torch.utils.data import ConcatDataset, DataLoader 
+
+from text_recognizer import util 
+from text_recognizer.data.util import BaseDataset
+import text_recognizer.metadata.shared as metadata 
+
 def load_and_print_info():
   pass
