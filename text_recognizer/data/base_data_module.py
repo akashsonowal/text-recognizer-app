5ac351710f60dacff97d715d5365bf0136632258
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-03 09:05:44 +0530

    update

--- text_recognizer/data/base_data_module.py
+++ text_recognizer/data/base_data_module.py
@@ -12,5 +12,12 @@ from text_recognizer import util
 from text_recognizer.data.util import BaseDataset
 import text_recognizer.metadata.shared as metadata 
 
-def load_and_print_info():
-  pass
+def load_and_print_info(data_module_class) -> None:
+  """Load EMNISTLines and print info."""
+  parser = argparse.ArgumentParser()
+  data_module_class.add_to_argparse(parser)
+  args = parser.parse_args()
+  dataset = data_module_class(args)
+  dataset.prepare_data()
+  dataset.setup()
+  print(dataset)
