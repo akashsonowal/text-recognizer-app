8319d1178f5fa8379d7b7ed1707da122c79a858f
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-03 18:05:58 +0530

    update

--- text_recognizer/data/emnist.py
+++ text_recognizer/data/emnist.py
@@ -1,7 +1,30 @@
+"""EMNIST dataset. Downloads from NIST website and saves as .npz file if not already present."""
+import json
+import os 
+from pathlib import Path 
+import shutil
+from typing import Sequence 
+import zipfile 
+
+import h5py
+import numpy as np 
+import toml 
+
+from text_recognizer.data.base_data_module import _download_raw_dataset, BaseDataModule, load_and_print_info
+from text_recognizer.data.util import BaseDataset, split_dataset
+import text_recognizer.metadata.emnist as metadata
 from text_recognizer.util import temporary_working_directory
+
+class EMNIST(BaseDataModule):
+    def __init__(self, args=None):
+        super().__init__(args)
+
+
 
 def _process_raw_dataset(filename: str, dirname: Path):
     print("Unzipping EMNIST...")
     with temporary_working_directory(dirname):
         from scipy.io import loadmat
 
+if __name__ == "__main__":
+    load_and_print_info(EMNIST)
