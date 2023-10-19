0e0894e876a10ab2d343f320b5c89705d49a4510
Akash Sonowal <work.akashsonowal@gmail.com>
2023-10-19 22:44:11 +0530

    Update iam.py

--- text_recognizer/metadata/iam.py
+++ text_recognizer/metadata/iam.py
@@ -1,9 +1,9 @@
-import text_recognizer.metadata.shared as shared 
+import text_recognizer.metadata.shared as shared
 
 RAW_DATA_DIRNAME = shared.DATA_DIRNAME / "raw" / "iam"
-METADATA_FILENAME = RAW_DATA_DIRNAME /  "metadata.toml"
+METADATA_FILENAME = RAW_DATA_DIRNAME / "metadata.toml"
 DL_DATA_DIRNAME = shared.DATA_DIRNAME / "downloaded" / "iam"
 EXTRACTED_DATASET_DIRNAME = DL_DATA_DIRNAME / "iamdb"
 
-IMAGE_SCALE_FACTOR = 2  # if images were downsampled, the regions must also be
-LINE_REGION_PADDING = 8 # add this many pixels around the exact coordinates
+DOWNSAMPLE_FACTOR = 2  # if images were downsampled, the regions must also be
+LINE_REGION_PADDING = 8  # add this many pixels around the exact coordinates
